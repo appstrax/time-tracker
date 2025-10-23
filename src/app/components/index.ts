60a77a14f1661268f1c19614c058ee6984864081
Cam <62012343+Cam-AS@users.noreply.github.com>
2025-10-23 09:50:46 +0200

    Split the tickets page with vertical split

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -4,6 +4,7 @@ export * from './history/history.component';
 export * from './side-nav/side-nav.component';
 export * from './split-pane/split-pane.component';
 export * from './breadcrumbs/breadcrumbs.component';
+export * from './kanban-board/kanban-board.component';
 export * from './project-selector/project-selector.component';
 export * from './side-nav/expanded/side-nav-expanded.component';
 export * from './side-nav/collapsed/side-nav-collapsed.component';
